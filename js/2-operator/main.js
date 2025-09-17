edba450d04b66586758d1e18829471b7e068009b
Yimin2 <gownfla14@naver.com>
2025-09-17 15:42:27 +0900

    feat(ui): JS 형변환 및 불리언 연산 예제 추가
    - 다양한 형변환 예제 추가
    - 불리언 연산 및 암시적 형변환 설명
    - 코드 가독성 향상

--- js/2-operator/main.js
+++ js/2-operator/main.js
@@ -17,3 +17,42 @@ console.log(`true || false = ${true || false}`);
 
 console.log(`!true = ${!true}`);
 console.log(`!false = ${!false}`);
+
+// == vs === != vs !==
+const number = 1;
+const string = '1';
+
+console.log(`number: ${typeof number}, string: ${typeof string}`);
+console.log(`number == string: ${number == string}`);
+console.log(`number === string: ${number === string}`);
+console.log(`number != string: ${number != string}`);
+console.log(`number !== string: ${number !== string}`);
+
+console.log(String(123)); // "123"
+
+String(true);
+String(undefined);
+String(null);
+
+console.log(`Number ${Number("123") == 123}`);
+console.log(Number("123abc")); // NaN
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
+console.log(Number(null)); // 0
+console.log(Number(undefined)); // NaN
+
+console.log(`Boolean ${Boolean(1)}`); // true
+console.log(Boolean(0)); // false
+console.log(Boolean("hello")); // true
+console.log(Boolean("")); // false
+console.log(Boolean("      ")); // true
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(NaN)); // false
+
+console.log(`암시적 형변환 ${!false}`); // true`
+console.log(!0); // true
+console.log(!""); // true
+console.log(!1); // false
+
+console.log(1 && 0);
