1542edb300d4c3872dd497c64cce90390a23d35b
Yimin2 <gownfla14@naver.com>
2025-09-17 11:51:55 +0900

    feat(ui): JS 연산자 학습 예제 및 HTML 템플릿 추가
    - JavaScript 연산자 관련 학습 예제 코드 추가 (main.js)
    - 자료형 확인, 변수 선언, 템플릿 리터럴 등 예제 코드 추가
    - index.html 파일 추가 및 기본 HTML 템플릿 작성
    - IntelliJ IDEA 관련 설정 파일(.idea) 추가
    - settings.local.json에 서버 비활성화 항목 추가

---
+++ js/2-operator/main.js
@@ -0,0 +1,19 @@
+// 타입 연산자
+// typeof
+
+// 산술 연사자
+// + - * / % **
+
+// 비교 연산자
+// < > <= >= == != === !==
+
+// 논리 연산자
+// && || !
+console.log(`true && true = ${true && true}`);
+console.log(`true && false = ${true && false}`);
+
+console.log(`true || true = ${true || true}`);
+console.log(`true || false = ${true || false}`);
+
+console.log(`!true = ${!true}`);
+console.log(`!false = ${!false}`);
