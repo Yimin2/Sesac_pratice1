8424211c9190cd76f7a151cbd6f0a2f65076d94f
Yimin2 <gownfla14@naver.com>
2025-09-22 11:36:25 +0900

    feat: 기본 계산기 기능 추가 및 수학 함수 구현
    - add, subtract, multiply, divide 함수 추가
    - calculator.js 파일 생성 및 기본 계산기 객체 구현
    - main.js에서 계산기 기능 테스트
    - .gitignore 파일에 node_modules 추가

---
+++ js/7-math/main.js
@@ -0,0 +1,11 @@
+import { add, subtract, multiply, divide} from "./math.js";
+import  calobj from "./calculator.js";
+
+console.log(add(1,2))
+console.log(subtract(1,2))
+console.log(multiply(1,2))
+console.log(divide(1,2))
+
+console.log(calobj.add(3,4))
+console.log(calobj.subtract(3,4))
+console.log(calobj.multiplyArrow(3,4))
