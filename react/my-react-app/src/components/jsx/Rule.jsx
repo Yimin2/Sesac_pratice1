6f4bf168656ba8534724ed32c8ec27de362598b0
Yimin2 <gownfla14@naver.com>
2025-09-26 17:27:05 +0900

    feat: 새로운 컴포넌트 및 스타일 추가
    - Footer, Header, Interpolation, MyButton, MyList, Rule 컴포넌트 생성
    - App.module.css 파일에 스타일 추가
    - XML 파일 추가 및 마이그레이션 상태 설정

---
+++ react/my-react-app/src/components/jsx/Rule.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Rule() {
+  return (
+    <>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+
+      <p style={{ fontSize: "3rem", color: "blue" }}>P 태그 {2 + 2}</p>
+    </>
+  );
+}
