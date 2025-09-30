193d4140ed591be9e8184bd3d60a58c747cded6a
Yimin2 <gownfla14@naver.com>
2025-09-30 17:15:39 +0900

    feat(state): 상태 사용 예제 컴포넌트 추가
    - StateBasic.jsx: 상태(state) 관리 및 배열 추가 기능 구현
    - Container.jsx: StateBasic 컴포넌트를 포함하는 컨테이너 컴포넌트 생성

---
+++ react/my-react-app/src/components/State/Container.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StateBasic from "./StateBasic.jsx";
+
+function Container(props) {
+  return (
+    <div>
+      <StateBasic />
+    </div>
+  );
+}
+
+export default Container;
