0fb27982da2cd8015875309f9202f876ef931a1f
Yimin2 <gownfla14@naver.com>
2025-10-14 10:44:56 +0900

    feat(router): 포스트 정렬 기능 추가
    - PostList.jsx: 정렬 상태 관리 및 useSearchParams 활용 로직 추가
    - id 오름차순/내림차순 버튼 및 정렬별 요청 처리 구현
    - axios 요청 URL에 정렬 조건 동적 적용
    - React Router의 URLSearchParams와 상태 연동

--- react/react-router-app/src/pages/RootPages/PostList.jsx
+++ react/react-router-app/src/pages/RootPages/PostList.jsx
@@ -1,24 +1,52 @@
 import React, {useEffect, useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 import axios from "axios";
 
 function PostList(props) {
     const [posts, setPosts] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    function orderByIdAsc() {
+        setSearchParams({
+            order: "asc", sortBy: "id"
+        })
+    }
+
+    function orderByIdDesc() {
+        setSearchParams({
+            order: "desc", sortBy: "id"
+        })
+    }
+
     useEffect(() => {
+        const params = searchParams;
+        const order = params.get("order") ?? "asc"
+        const sortBy = params.get("sortBy") ?? "id"
+
+
         async function getPosts() {
-            const response = await axios.get("https://dummyjson.com/posts")
+            const response = await axios.get(`https://dummyjson.com/posts?sortBy=${sortBy}&order=${order}`)
             setPosts(response.data.posts)
         }
 
         getPosts()
-    }, []);
+    }, [searchParams]);
 
     return (<div>
+        <div className="flex gap-2">
+            <div className="border-2 p-2 cursor-pointer"
+                 onClick={() => orderByIdAsc()}>id 오름차순
+            </div>
+            <div className="border-2 p-2 cursor-pointer"
+                 onClick={() => orderByIdDesc()}>id 내림차순
+            </div>
+        </div>
         {posts.map((post) => {
-            return (<Link key={post.id} to={`/posts/${post.id}`}>
-                    No.{post.id} - {post.title}
+            return (<Link key={post.id}
+                          to={`/posts/${post.id}`}>
+                No.{post.id} - {post.title}
                 <br/>
-                </Link>)
+            </Link>)
         })}
     </div>);
 }
